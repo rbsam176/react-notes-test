b09f055e0b905e33e032c920570f423ae520f444
VU City <vucity@Sams-MacBook-Pro-2.local>
2022-10-21 09:54:06 +0100

    formatted last modified date string

--- src/components/Sidebar/ListItem.js
+++ src/components/Sidebar/ListItem.js
@@ -1,16 +1,24 @@
 import './ListItem.css';
 
 export default function ListItem(props) {
-    return (
+  const date = new Date(props.note.lastModified)
+  const today = new Date().toDateString()
+  let dateFormatted = date.toGMTString()
+
+  if (today === date.toDateString()) {
+    dateFormatted = `Today at ${date.toLocaleTimeString()}`
+  }
+
+  return (
       <ul>
         <li className={props.activeNote === props.note.id ? 'active' : ''} onClick={() => props.setActiveNote(props.note.id)}>
             <div className="item-content">
                 <strong className="title">{props.note.title}</strong>
                 <span className="summary">{props.note.content}</span>
-                <small>Last modified: {props.note.lastModified}</small>
+                <small>Last modified: {dateFormatted}</small>
             </div>
             <button className="delete-note" onClick={() => props.onDeleteNote(props.note.id)}>Delete</button>
         </li>
       </ul>
-    );
+  );
 }
